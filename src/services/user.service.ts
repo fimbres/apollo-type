b28bf1653b8b355195a1550cd8a7414dba066f96
Isaac <fimbresisaac23@gmail.com>
2023-01-11 23:44:38 -0800

    new auth methods

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,37 @@
-import { UserModel } from "../schema/user.schema";
+import { ApolloError } from "apollo-server";
+import bcrypt from "bcrypt";
+
+import { LoginInput, UserModel } from "../schema/user.schema";
+import Context from "../types/context";
+import { signJwt } from "../utils/jwt";
 
 class UserService {
     async createUser(input: any) {
         return UserModel.create(input);
+    }
+
+    async login (input: LoginInput, context: Context){
+        const user = await UserModel.find().findByEmail(input.email).lean();
+        
+        if(!user) {
+            throw new ApolloError('Invalid email');
+        }
+
+        const passwordIsValid = await bcrypt.compare(input.password, user.password);
+
+        if(!passwordIsValid) {
+            throw new ApolloError('Invalid password');
+        }
+
+        const token = signJwt(user);
+
+        context.res.cookie("accessToken", token, {
+            maxAge: 3.154e10,
+            path: "/",
+            secure: process.env.NODE_ENV === "production"
+        })
+
+        return token;
     }
 }
 
